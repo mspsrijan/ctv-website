b581c3e5e6fe5e5a220ec87ce447176f45b25d4d
Shah Paran Srijan <mspsrijan@gmail.com>
2024-08-31 02:40:43 +0600

    Modified language switcher for Github directory

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const path = window.location.pathname.split("/");
 
   // If the URL doesn't have a language code, redirect to the default language
-  if (!path[1] || path[1].length !== 2) {
+  if (!path[2] || path[2].length !== 2) {
     window.location.pathname = `/en${window.location.pathname}`;
   }
 });
 
 function switchLanguage(lang) {
   const path = window.location.pathname.split("/");
-  path[1] = lang;
+  path[2] = lang;
   window.location.pathname = path.join("/");
 }
 
